71dfbfed6b7ffaa030a2a0cb7b8f1da84c514828
CsSzilveszter <csordasszili@gmail.com>
2023-12-28 10:32:40 +0100

    Delete metódus hozzáadása.

--- MeasureConverter/src/main/java/com/example/measureconverter/Converter.java
+++ MeasureConverter/src/main/java/com/example/measureconverter/Converter.java
@@ -1,4 +1,51 @@
 package com.example.measureconverter;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Converter {
+
+    @FXML
+    private TextField Kilometer;
+
+    @FXML
+    private TextField Merfold;
+
+    @FXML
+    private TextField KilometerPerOra;
+
+    @FXML
+    private TextField MerfoldPerOra;
+
+    @FXML
+    private TextField Kilogramm;
+
+    @FXML
+    private TextField Font;
+
+    @FXML
+    private TextField Celsius;
+
+    @FXML
+    private TextField Fahrenheit;
+
+
+    @FXML
+    private Button logoutbutton;
+
+
+    @FXML
+    protected void Delete() {
+        Kilometer.setText("");
+        Merfold.setText("");
+        KilometerPerOra.setText("");
+        MerfoldPerOra.setText("");
+        Kilogramm.setText("");
+        Font.setText("");
+        Celsius.setText("");
+        Fahrenheit.setText("");
+    }
+
+
 }
