e7e9bc3c5c255b2f186dbc29d9c00579feb6848f
CsSzilveszter <csordasszili@gmail.com>
2023-12-28 10:46:13 +0100

    Kilométer-mérföld átváltás hozzáadása.

--- MeasureConverter/src/main/java/com/example/measureconverter/Converter.java
+++ MeasureConverter/src/main/java/com/example/measureconverter/Converter.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
+import java.util.Objects;
+
 public class Converter {
 
     @FXML
@@ -47,5 +49,47 @@ public class Converter {
         Fahrenheit.setText("");
     }
 
+    protected void KilometerToMerfold()
+    {
+        if (Objects.equals(Kilometer.getText(), ""))
+        {
+            Merfold.setText("");
+        }
+        else
+        {
+            try
+            {
+                double kilometer = Double.parseDouble(Kilometer.getText());
+                double merfold = kilometer * 0.621371;
+                merfold = Math.round(merfold * 100.0) / 100.0;
+                Merfold.setText(String.valueOf(merfold));
+            }
+            catch (NumberFormatException ex)
+            {
+                Kilometer.requestFocus();
+            }
+        }
+    }
+
+    protected void MerfoldToKilometer() {
+        if (Objects.equals(Merfold.getText(), ""))
+        {
+            Kilometer.setText("");
+        }
+        else
+        {
+            try
+            {
+                double merfold = Double.parseDouble(Merfold.getText());
+                double kilometer = merfold * 1.609344;
+                kilometer = Math.round(kilometer * 100.0) / 100.0;
+                Kilometer.setText(String.valueOf(kilometer));
+            }
+            catch (NumberFormatException ex)
+            {
+                Merfold.requestFocus();
+            }
+        }
+    }
 
 }
